7381d679e68b0f3ce181206c6f83e795cd40daf6
MARVELG0D <bukan.marvel69@gmail.com>
2026-02-24 17:36:39 +0700

    pmd fix final

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -15,6 +15,10 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public Product create(Product product) {
         return productRepository.create(product);
